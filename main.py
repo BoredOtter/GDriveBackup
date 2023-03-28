9d3b6c8a2c206204afba6ae9bd28d87e7d6bef06
Eryk Stańdo <eryk.stando@gmail.com>
2023-03-28 15:21:12 +0200

    second commit

--- main.py
+++ main.py
@@ -6,52 +6,43 @@ import os
 
 root = Tk()
 root.withdraw()
-#acvalhlalla ID: 1KP9K5aXPOicSUFkd9x97ccnEiwROHvsn
 
+#google authentication
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
 
-#folder_list = drive.ListFile({'q': "trashed=false"}).GetList()
+#choosing path
+path = filedialog.askdirectory(title='Choose folder to Backup')
 
-#for folder in folder_list:
-#    print ('folder title: %s, id: %s' % (folder['title'], folder['id']))
-print("window")
+folder = os.path.split(path)
+folderName=folder[1]
 
-
-folder_selected = filedialog.askdirectory(title='Choose folder to Backup')
-path = folder_selected
-print("windo2w")
-#path = r"C:\Users\eryks\AppData\Roaming\Goldberg UplayEmu Saves\13504"
 print("Path: ",path)
+print("Folder: ",folderName)
 
 
 numberOfFiles=0
 for entry in os.listdir(path):
     if os.path.isfile(os.path.join(path, entry)):
-        #print(entry)  #list of files in directory
+        print("    ",entry)  #list of files in directory
         numberOfFiles+=1
 
 print("Number of files: ",numberOfFiles)
 
-dirs = os.listdir( path )
-
-folder_id = 'parent_folder_id'
-
-folder_metadata = {'title' : 'MyFolder', 'mimeType' : 'application/vnd.google-apps.folder'}
+#crating a folder with the same name as choosen one
+folder_metadata = {'title' : folderName, 'mimeType' : 'application/vnd.google-apps.folder'}
 folder = drive.CreateFile(folder_metadata)
 folder.Upload()
+folderID=folder['id']
 
-# This would print all the files and directories
+#uploading files to drive
+dir = os.listdir(path)
+for file in dir:
+    print("    Uploading: ",file)
+    File = drive.CreateFile({'title': file,'parents': [{'id': folderID}]})
+    File.SetContentFile(path+ "\\" + file)
+    File.Upload()
 
-#numberOfFiles = 
-
-# # for file in dirs:
-# #    print(file)
-# #    file1 = drive.CreateFile({'title': file,'parents': [{'id': '1KP9K5aXPOicSUFkd9x97ccnEiwROHvsn'}]})
-# #    file1.SetContentFile(path+ "\\" + file)
-# #    file1.Upload()
-
-# # print("Number of files: ",file.__len__())
 
 
